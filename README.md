ac1c76b69e89c739674d350af37a5d7e4b5d5d04
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-20 12:21:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS20-PWA-Test
+
+[![GitHub's Super Linter](https://github.com/marshall-demars/ICS20-PWA-Test/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/marshall-demars/ICS20-PWA-Test/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/marshall-demars/ICS20-PWA-Test)](https://repl.it/github/marshall-demars/ICS20-PWA-Test)
+
+This site can be found at: [https://marshall-demars.github.io/ICS20-PWA-Test](https://marshall-demars.github.io/ICS20-PWA-Test)
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
